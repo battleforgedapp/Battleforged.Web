dbc024a84ae05d5d1d71270b45400fa093d96747
Stuart Harrison <stuartaharrison3@gmail.com>
2024-04-07 14:00:28 +0100

    added testing logic for clerk & YARP

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,16 +1,70 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import http from "./http-common";
+import { SignedIn, SignedOut, SignInButton, UserButton, useAuth } from "@clerk/clerk-react";
+import { useQuery } from '@tanstack/react-query'
+
+
+
+const Armies = () => {
+  const { getToken } = useAuth();
+  const { isPending, error, data, isFetched } = useQuery({ 
+    queryKey: ['testData'],
+    queryFn: async () => {
+      const token = await getToken();
+      console.log('token', token);
+      const response = await http.get('army-service/armies', {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+      });
+      return response.data;
+    }
+  })
+
+  /*
+const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      axios
+        .get('https://api.github.com/repos/tannerlinsley/react-query')
+        .then((res) => res.data),
+  })
+  */
+
+  return (
+    <div>
+      {isPending && (<p>Pedning...</p>)}
+      {error && (<p>Oops! There was a problem!</p>)}
+      {data && (<p>{JSON.stringify(data)}</p>)}
+    </div>
+  );
+};
 
 const App = () => {
+  const { isSignedIn } = useAuth();
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <div>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <body>
+        {!isSignedIn && <p>You need to login to see anything!</p>}
+        {isSignedIn && (<Armies />)}
+      </body>
+    </div>
   )
 };
+
+
+
+// const getArmies = async () => {
+//   return axios.get('http://localhost:5116/army-service/armies', {
+
+//   })
+// }
 
 export default App;
--- src/src/main.tsx
+++ src/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
- 
+
+const queryClient = new QueryClient();
+
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
@@ -13,7 +16,9 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ClerkProvider>
   </React.StrictMode>,
 )
